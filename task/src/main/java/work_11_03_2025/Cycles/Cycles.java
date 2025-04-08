e01388533ed93170a29d4abf8ce454776aa07dd9
Pavel2005-oss <mazalovp@.list.ru>
2025-04-08 13:35:31 +0300

    multiplication table has been added

--- task/src/main/java/work_11_03_2025/Cycles/Cycles.java
+++ task/src/main/java/work_11_03_2025/Cycles/Cycles.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Cycles {
     public static void main(String[] args) {
         cycles();
+        table();
 
     }
 
@@ -28,4 +29,20 @@ public class Cycles {
         System.out.println("результат -> " + n);
 
     }
+    /**
+     * С помощью вложенных циклов for выведите таблицу умножения от 1 до 10.
+     *Оформите вывод в виде таблицы.
+     */
+    public static void table() {
+        System.out.println("Таблица умножения от 1 до 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%4d ", i * j);
+            }
+            System.out.println();
+
+        }
+    }
+
 }

4349a132070e2c1edbf90bed9d3104b3ffd58507
Pavel2005-oss <mazalovp@.list.ru>
2025-04-10 19:41:50 +0300

    Reverse number

---
+++ task/src/main/java/work_13_03_2025/Task_3.java
@@ -0,0 +1,37 @@
+package work_13_03_2025;
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число строк: ");
+        int rows = scanner.nextInt();
+        System.out.print("Введите число столбцов: ");
+        int cols = scanner.nextInt();
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Введите элементы матрицы:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Суммы по строкам:");
+        for (int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                rowSum += matrix[i][j];
+            }
+            System.out.println("Строка " + i + ": " + rowSum);
+        }
+        System.out.println("Суммы по столбцам:");
+        for (int j = 0; j < cols; j++) {
+            int colSum = 0;
+            for (int i = 0; i < rows; i++) {
+                colSum += matrix[i][j];
+            }
+            System.out.println("Столбец " + j + ": " + colSum);
+        }
+
+        scanner.close();
+    }
+}

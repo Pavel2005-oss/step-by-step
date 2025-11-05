464686cdbd626b74255ea16d36a8e237d2cddfda
Pavel2005-oss <mazalovp@.list.ru>
2025-11-05 18:50:27 +0300

    дз  по циклам

---
+++ task/src/main/java/work_01_11_25/Practical_tasks_1.java
@@ -0,0 +1,36 @@
+public class Practical_tasks_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void task_1(String[] args) {
+        /// Уровень 1: Базовые задания
+
+        ///Задание 1: Вывод чисел
+        ///Напишите программу, которая выводит числа от 1 до 10.
+//              **Подсказка:** Используйте цикл `for`.
+        int i = 1;
+            for (i = 1; i = 10; i++){
+                System.out.println(i);
+            }
+     }
+
+    public static void task_2(String[] args) {
+///        #### Задание 2: Таблица умножения
+///         Выведите таблицу умножения на 5 (от 5×1 до 5×10).
+///
+///            **Ожидаемый результат:**
+///           ```
+///        5 x 1 = 5
+///        5 x 2 = 10
+///            ...
+///        5 x 10 = 50
+
+        int n =5;
+        for (n = 5; n = 50; n * 5){
+            System.out.println();
+        }
+
+    }
+
+}

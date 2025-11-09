b0d83b31bb6f085165ef44ad13bb09047927e363
Pavel2005-oss <mazalovp@.list.ru>
2025-11-09 21:22:36 +0300

    вывод числа от десяти до одного

---
+++ task/src/main/java/work_01_11_25/task_3.java
@@ -0,0 +1,12 @@
+package work_01_11_25;
+
+public class task_3 {
+    /*Задание 3: Обратный отсчёт
+Напишите программу, которая выводит числа от 10 до 1 в обратном порядке.
+     */
+    public static void main(String[] args) {
+        for (int i = 10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
